dfae276b945e17577593cd0512fe3e9862b858e7
Josh <josh.crane512@gmail.com>
2023-01-06 11:30:56 +0800

    No need for dot

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar
 tar xzvf nvim-linux64.tar.gz
 cp ./nvim-linux64/bin/nvim /usr/local/bin/nvim
 
-cp tmux/.tmux.conf ~/.tmux.conf
+cp tmux/tmux.conf ~/.tmux.conf
 cp -rf ~/.config/nvim; mv nvim ~/.config/
 cp zsh/.zshrc ~/.zshrc
 
