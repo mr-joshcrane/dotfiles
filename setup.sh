513b572061541e9bf0dd913f371b1486a3fe1563
Josh <josh.crane512@gmail.com>
2023-01-06 11:07:23 +0800

    forcing replacement of old config

--- setup.sh
+++ setup.sh
@@ -6,8 +6,8 @@ wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar
 tar xzvf nvim-linux64.tar.gz
 mv ./nvim-linux64/bin/nvim /usr/local/bin/nvim
 
-mv .tmux.conf ~/.tmux.conf
-mv nvim ~/.config
+mv tmux/.tmux.conf ~/.tmux.conf
+rm -rf ~/.config/nvim; mv nvim ~/.config/
 mv zsh/.zshrc ~/.zshrc
 
 
