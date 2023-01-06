c62926e6b083b920caffdb9d13c67db50a9d6f28
Josh <josh.crane512@gmail.com>
2023-01-06 11:03:58 +0800

    removing execute vim

--- setup.sh
+++ setup.sh
@@ -4,7 +4,6 @@ sudo apt-get install tmux -y
 
 wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
 tar xzvf nvim-linux64.tar.gz
-./nvim-linux64/bin/nvim
 mv ./nvim-linux64/bin/nvim /usr/local/bin/nvim
 
 mv .tmux.conf ~/.tmux.conf
