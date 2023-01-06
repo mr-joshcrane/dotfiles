c45c77642bfbe8d301a67ed4db76866292e0464f
mr-joshcrane <57082182+mr-joshcrane@users.noreply.github.com>
2023-01-06 11:19:59 +0800

    same for tmux

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar
 tar xzvf nvim-linux64.tar.gz
 mv ./nvim-linux64/bin/nvim /usr/local/bin/nvim
 
-mv tmux/.tmux.conf ~/.tmux.conf
+mv tmux/tmux.conf ~/.tmux.conf
 rm -rf ~/.config/nvim; mv nvim ~/.config/
 mv zsh/zshrc ~/.zshrc
 
