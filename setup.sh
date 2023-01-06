b6169775bef72f7d98cac3b5cfbabb595b183052
Josh <josh.crane512@gmail.com>
2023-01-06 12:38:57 +0800

    cleanup and tpm

--- setup.sh
+++ setup.sh
@@ -1,12 +1,14 @@
 #!/bin/zsh
 chsh -s $(which zsh)
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 sudo apt-get update && sudo apt-get install tmux -y
 
 wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
 tar xzvf nvim-linux64.tar.gz
 cp ./nvim-linux64/bin/nvim /usr/local/bin/nvim
+rm -rf nvim-linux64 nvim-linux64.tar.gz
 
 cp tmux/tmux.conf ~/.tmux.conf
 rm -rf ~/.config/nvim; cp -R nvim ~/.config/
