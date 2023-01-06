00ddd5170be59442942501e1ce22c28a1cc75225
Josh <josh.crane512@gmail.com>
2023-01-06 11:49:52 +0800

    Adding power level config

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
 sudo apt-get update && sudo apt-get install tmux -y
 
 wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
@@ -10,5 +11,6 @@ cp ./nvim-linux64/bin/nvim /usr/local/bin/nvim
 cp tmux/tmux.conf ~/.tmux.conf
 rm -rf ~/.config/nvim; cp -R nvim ~/.config/
 cp zsh/zshrc ~/.zshrc
+cp powerlevel10k/ ~/powerlevel10k/
 
 . ~/.zshrc
