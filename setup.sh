55f5f610ef9b43661d071a71c54c5348150e6c65
Josh <josh.crane512@gmail.com>
2023-01-06 12:06:32 +0800

    adding default shell as zsh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-
+chsh -s $(which zsh)
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 sudo apt-get update && sudo apt-get install tmux -y
